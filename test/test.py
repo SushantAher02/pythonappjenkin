3f0412d433f5f674c6445161c45c3afacb53f966
sushantaher <ahersuahnt@gmail.com>
2025-01-25 13:31:38 +0530

    fivecommit

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
         tester = app.test_client()
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"SushantAher")
+        self.assertEqual(response.data, b"SushantAherPatil")
 
 
 if __name__ == "__main__":
